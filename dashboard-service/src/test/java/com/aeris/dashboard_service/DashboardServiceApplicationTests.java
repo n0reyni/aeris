686c29d7faaa91b7e06e922bebd6548f4701eafa
Your Name <you@example.com>
2026-06-04 23:23:12 +0000

    base setup

---
+++ dashboard-service/src/test/java/com/aeris/dashboard_service/DashboardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeris.dashboard_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
