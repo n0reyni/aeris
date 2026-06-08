5a0ed04197f3c1678c390a11c85c57822f78e781
n0reyni <ousmanesow7@esp.sn>
2026-06-08 22:25:26 +0000

    correcting ui

--- auth-service/src/main/java/com/aeris/auth_service/service/AuthService.java
+++ auth-service/src/main/java/com/aeris/auth_service/service/AuthService.java
@@ -46,7 +46,7 @@ public class AuthService {
     public AuthResponse login(LoginRequest request) {
         User user = userRepository
             .findByEmail(request.getEmail())
-            .orElseThrow(() -> new RuntimeException("invalid credentials"));
+            .orElseThrow(() -> new RuntimeException("Invalid Credentials"));
 
         if (
             !passwordEncoder.matches(request.getPassword(), user.getPassword())
